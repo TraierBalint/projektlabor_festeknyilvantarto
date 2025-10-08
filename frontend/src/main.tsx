03d09164ad39b62d8aad108e65682d3ddc104274
TraierBalint <balintka2016@gmail.com>
2025-10-08 11:45:30 +0200

    Yarn + Vite integráció teljes és hibamentes

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,14 @@
+import "@mantine/core/styles.css";
+import { MantineProvider } from "@mantine/core";
+import { theme } from "./theme";
+import { BrowserRouter } from "react-router-dom";
+import { AuthContext } from "./context/AuthContext";
+import Routing from "./routing/Routing";
+import { useState } from "react";
+
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
+import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
